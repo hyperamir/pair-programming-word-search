00f52bf26d22da4304e3ec4bae2bfb5c34f1483a
Amir Sedaghat <amir.sedaghat@gmail.comgit config --global user.email amir.sedaghat@gmail.com>
2021-12-02 01:33:33 +0000

    adding vertical search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -9,15 +9,15 @@ describe("#wordSearch()", function() {
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['H', 'M', 'J', 'R', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'A', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'N', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'K', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'FRANK')
 
-    assert.isFalse(result);
+    assert.isTrue(result);
   });
 
   it("should return true if the word is present", function() {
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
+
+    const newArray = [];
+    let tempArr = [];
+    for (let row = 0; row < letters[0].length; row++) {
+        for (let col = 0; col < letters.length; col++) {
+            tempArr.push(letters[col][row]);
+        }
+        newArray.push(tempArr);
+        tempArr = [];
+    }
+    const verticalJoin = newArray.map(le => le.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    return false;
 }
 
 module.exports = wordSearch
